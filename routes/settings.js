fcf84510f3647525792d6347c0954f7deafaa14a
mikem48 <morel.m48@gmail.com>
2026-01-17 19:43:15 +0100

    Update database path in settings route

--- routes/settings.js
+++ routes/settings.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const db = require('/nastavitve.db'); // tvoja baza
+const db = require('../database2'); // tvoja baza
 
 // Helper za checkbox -> boolean
 const castBool = v => v === 'on';
