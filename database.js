fbef48234ba84fdc95286348c5a273f5c9e8c226
mikem48 <morel.m48@gmail.com>
2026-01-17 19:30:26 +0100

    Modify database.js to export multiple database instances
    Updated database module to export two database instances.

--- database.js
+++ database.js
@@ -1,4 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./esp32.db');
-
-module.exports = db;
+const db1 = new sqlite3.Database('./esp32.db');
+const db2 = new sqlite3.Database('./nastavitve.db');
+module.exports = db1;
+module.exports = db2;
