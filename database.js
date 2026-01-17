50c9e3fffc60ad415f1f8ac06e6a5f45446c4820
mikem48 <morel.m48@gmail.com>
2026-01-17 19:42:11 +0100

    Refactor database exports to use a single database

--- database.js
+++ database.js
@@ -1,5 +1,4 @@
 const sqlite3 = require('sqlite3').verbose();
-const db1 = new sqlite3.Database('./esp32.db');
-const db2 = new sqlite3.Database('./nastavitve.db');
-module.exports = db1;
-module.exports = db2;
+const db = new sqlite3.Database('./esp32.db');
+module.exports = db;
+
