cc8bdc89ec2b05312670da8c13317fd7311df652
mikem48 <morel.m48@gmail.com>
2026-03-04 16:50:37 +0100

    Add new columns to database2.js schema

--- database2.js
+++ database2.js
@@ -10,6 +10,30 @@ db.serialize(() => {
       last_update INTEGER,
       firmware_version TEXT,
       params TEXT
+      
+      visina REAL DEFAULT 0,
+      wifi_cas INTEGER DEFAULT 60,
+      obvestilo_napetost REAL DEFAULT 12.5,
+      obvestilo_krmilo REAL DEFAULT 80,
+      obvestilo_stevilka TEXT DEFAULT '',
+      
+      -- Krmilnica
+      ura1_h INTEGER DEFAULT 0,
+      ura1_min INTEGER DEFAULT 0,
+      ura2_h INTEGER DEFAULT 0,
+      ura2_min INTEGER DEFAULT 0,
+      casovnik2 INTEGER DEFAULT 0,
+      cas_delovanja INTEGER DEFAULT 60,
+      hitrost_motorja INTEGER DEFAULT 100,
+      
+      -- Dnevi
+      pon INTEGER DEFAULT 0,
+      tor INTEGER DEFAULT 0,
+      sre INTEGER DEFAULT 0,
+      cet INTEGER DEFAULT 0,
+      pet INTEGER DEFAULT 0,
+      sob INTEGER DEFAULT 0,
+      ned INTEGER DEFAULT 0
     )
   `);
 
